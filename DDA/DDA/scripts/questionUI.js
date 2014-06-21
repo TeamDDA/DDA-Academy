9a93871a5bd7fac526ff020aae17cc391ba17069
delyan-nikolov-1992 <delyan.nikolov.1992@gmail.com>
2014-06-21 21:29:54 +0300

    Buttons Color

--- DDA/DDA/scripts/questionUI.js
+++ DDA/DDA/scripts/questionUI.js
@@ -11,49 +11,56 @@ function showQuestion(gameCharacter) {
             "e oniq pi4"
         ]
     };*/
-	var questionData;
-	
-	$.ajax({
-		type: 'POST',
-		url: 'php/getQuestionIDs.php',
-		data: { questionType: "1" }
-	}).done(function (data) {
-		var json = JSON.parse(data);
-		var randomIndex = Math.floor(Math.random() * json.length);
-		var questionId = parseInt(json[randomIndex]);
-		$.ajax({
-			type: 'POST',
-			url: 'php/getQuestion.php',
-			data: { type: 'questionId', questionId: questionId }
-		}).done(function (data) {
-			questionData = JSON.parse(data);
-			
-			var questionBox = document.getElementById('questionBox');
-			
-			while (questionBox.firstChild) {
-				questionBox.removeChild(questionBox.firstChild);
-			}
-			var questionTitle = document.createElement('h3');
-			questionTitle.innerHTML = questionData.question;
-			questionBox.appendChild(questionTitle);
-			var answerTemplate = document.createElement('button');
-			for (var i = 0; i < questionData.answers.length; i++) {
-				var currentAnswer = answerTemplate.cloneNode(true);
-				currentAnswer.innerHTML = questionData.answers[i];
-				currentAnswer.id = i + '-answer';
-				questionBox.appendChild(currentAnswer);
-				currentAnswer.addEventListener('click', function () {
-					if(this.id.substr(0,1) == questionData.correct){
-						PLAYER_SCORE += 10;
-						alert("YESSSSS");
-					}
-				})
-			}
-		});
-	});
+    var questionData;
 
-    
+    $.ajax({
+        type: 'POST',
+        url: 'php/getQuestionIDs.php',
+        data: { questionType: gameCharacter.characteristic + 1 }
+    }).done(function (data) {
+        var json = JSON.parse(data);
+        var randomIndex = Math.floor(Math.random() * json.length);
+        var questionId = parseInt(json[randomIndex]);
+        $.ajax({
+            type: 'POST',
+            url: 'php/getQuestion.php',
+            data: { type: 'questionId', questionId: questionId }
+        }).done(function (data) {
+            questionData = JSON.parse(data);
 
+            var answers = document.getElementsByClassName('answers');
 
+            for (var j = 0; j < 4; j++) {
+                answers[j].style.backgroundColor = 'yellow';
+            }
 
+            document.getElementById('questionBox').style.display = 'block';
+
+            var questionTitle = document.getElementById('question');
+            questionTitle.innerText = questionData.question;
+
+            for (var i = 0; i < questionData.answers.length; i++) {
+                var currentAnswer = document.getElementById(i + '-answer');
+                currentAnswer.innerText = questionData.answers[i];
+
+                currentAnswer.addEventListener('click', function () {
+
+                    for (var j = 0; j < 4; j++) {
+                        if (answers[j].id.substr(0, 1) == questionData.correct) {
+                            answers[j].style.backgroundColor = 'green';
+                        } else {
+                            answers[j].style.backgroundColor = 'red';
+                        }
+                    }
+
+                    if (this.id.substr(0, 1) == questionData.correct) {
+                        PLAYER_SCORE += 10;
+                        alert("YESSSSS");
+                    }
+
+                    // TODO DA SE IZPRINTVA INFORMACIQTA ZA VERNIQ OTGOVOR A NE YESSSSS
+                })
+            }
+        });
+    });
 }
