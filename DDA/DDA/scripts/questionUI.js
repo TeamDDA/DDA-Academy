880a3a3080ff74ce063d389125381b6a0d041f10
adimitrov93 <a.dimitrov93@gmail.com>
2014-06-21 19:58:58 +0300

    Added database support

--- DDA/DDA/scripts/questionUI.js
+++ DDA/DDA/scripts/questionUI.js
@@ -1,7 +1,7 @@
 function showQuestion(gameCharacter) {
 
     //Ajax
-    var questionData = {
+    /*var questionData = {
         question: "Who am I?",
         correct: 0,
         answers: [
@@ -10,22 +10,50 @@ function showQuestion(gameCharacter) {
             "ninja",
             "e oniq pi4"
         ]
-    };
+    };*/
+	var questionData;
+	
+	$.ajax({
+		type: 'POST',
+		url: 'php/getQuestionIDs.php',
+		data: { questionType: "1" }
+	}).done(function (data) {
+		var json = JSON.parse(data);
+		var randomIndex = Math.floor(Math.random() * json.length);
+		var questionId = parseInt(json[randomIndex]);
+		$.ajax({
+			type: 'POST',
+			url: 'php/getQuestion.php',
+			data: { type: 'questionId', questionId: questionId }
+		}).done(function (data) {
+			questionData = JSON.parse(data);
+			
+			var questionBox = document.getElementById('questionBox');
+			
+			while (questionBox.firstChild) {
+				questionBox.removeChild(questionBox.firstChild);
+			}
+			var questionTitle = document.createElement('h3');
+			questionTitle.innerHTML = questionData.question;
+			questionBox.appendChild(questionTitle);
+			var answerTemplate = document.createElement('button');
+			for (var i = 0; i < questionData.answers.length; i++) {
+				var currentAnswer = answerTemplate.cloneNode(true);
+				currentAnswer.innerHTML = questionData.answers[i];
+				currentAnswer.id = i + '-answer';
+				questionBox.appendChild(currentAnswer);
+				currentAnswer.addEventListener('click', function () {
+					if(this.id.substr(0,1) == questionData.correct){
+						PLAYER_SCORE += 10;
+						alert("YESSSSS");
+					}
+				})
+			}
+		});
+	});
 
-    var questionBox = document.getElementById('questionBox');
-    var questionTitle = document.createElement('h3');
-    questionTitle.innerHTML = questionData.question;
-    questionBox.appendChild(questionTitle);
-    var answerTemplate = document.createElement('button');
-    for (var i = 0; i < questionData.answers.length; i++) {
-        var currentAnswer = answerTemplate.cloneNode(true);
-        currentAnswer.innerHTML = questionData.answers[i];
-        currentAnswer.id = i + '-answer';
-        questionBox.appendChild(currentAnswer);
-        currentAnswer.addEventListener('click', function () {
-            if (this.id.substr(0, 1) == questionData.correct) {
-                PLAYER_SCORE += 10;
-            }
-        })
-    }
+    
+
+
+
 }
